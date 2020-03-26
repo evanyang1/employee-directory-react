db6ef13aa0cd76b6d729c7d31f38569d9f657781
Evan Yang <evanyang@pop-os.localdomain>
2020-03-26 01:31:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Form from "./components/Form"
+
+
+const App = () => (
+  <>
+    <h1 className="title">Employee Directory</h1>
+    <Form />
+  </> 
+)
+
+export default App
